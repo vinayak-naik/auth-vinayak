3ee266b53f94051a8411967d343f63fdc413b2e8
vinayak-naik <vinayaknaik2021@gmail.com>
2022-12-03 18:14:04 +0530

    first commit 3

--- pages/index.tsx
+++ pages/index.tsx
@@ -16,7 +16,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">Next.js Vinayak</a>
         </h1>
         <div>
           <button onClick={() => signIn("google")}>sign in</button>
